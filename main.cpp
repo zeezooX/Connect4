8674c748c5e72263d8ab5e3c9d2bfb724a39f1c6
Ziyad Eslam <ziyadeslam@hotmail.com>
2022-05-24 22:50:39 +0200

    moving declarations to header file

--- main.cpp
+++ main.cpp
@@ -1,12 +1,9 @@
 #include <windows.h>
 #include "algorithms.h"
+#include "main.h"
 #include <bits/stdc++.h>
 
 using namespace std;
-
-HANDLE hConsole;
-void setColor(bool red, bool green, bool blue, bool highlight);
-void setCursor(int column, int line);
 
 int main()
 {
